8c740b53be9cf42355af94220d5eafd167ef9599
rgeneaux <rgeneaux@gmail.com>
2023-07-20 18:02:58 +0200

    Pymodaq v4 compatibility

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ PLUGIN_NAME = f"pymodaq_plugins_{SHORT_PLUGIN_NAME}"
 
 from pathlib import Path
 
+if not SHORT_PLUGIN_NAME.isidentifier():
+    raise ValueError("'SHORT_PLUGIN_NAME = %s' is not a valid python identifier." % SHORT_PLUGIN_NAME)
+
 with open(str(Path(__file__).parent.joinpath(f'src/{PLUGIN_NAME}/VERSION')), 'r') as fvers:
     version = fvers.read().strip()
 
@@ -30,7 +33,7 @@ setupOpts = dict(
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Human Machine Interfaces",
         "Topic :: Scientific/Engineering :: Visualization",
-        "License :: CeCILL-B Free Software License Agreement (CECILL-B)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: User Interfaces",
@@ -42,7 +45,9 @@ setup(
     packages=find_packages(where='./src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    entry_points={'pymodaq.plugins': f'{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}'},
+    entry_points={'pymodaq.plugins': f'{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}',
+                  'pymodaq.pid_models': f"{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}",
+                  'pymodaq.extensions': f"{SHORT_PLUGIN_NAME} = {PLUGIN_NAME}"},
     install_requires=['toml', ]+config['plugin-install']['packages-required'],
     **setupOpts
 )
